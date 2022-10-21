f4b96bf2c432a81124e10c0f97fc8b3498642fed
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2022-10-21 10:45:20 +0300

    VaporStore

---
+++ VaporStore/VaporStore/Data/Models/Enum/CardType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaporStore.Data.Models.Enum
+{
+    public enum CardType
+    {
+        Debit,
+        Credit,
+
+    }
+}
---
+++ VaporStore/VaporStore/Data/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaporStore.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        public string Username { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Range(3,103)]
+        public int Age { get; set; }
+    }
+}
