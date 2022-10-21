09ec5503691851ad10f3d4a1aad6188980ca0fd4
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2022-10-21 10:55:48 +0300

    VaporStore

---
+++ VaporStore/VaporStore/Data/Models/Purchase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaporStore.Data.Models.Enum;
+
+namespace VaporStore.Data.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public PurchaseType Type { get; set; }
+
+        [Required]
+        public string ProductKey { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int CardId { get; set; }
+        public Card Card { get; set; }
+
+        [Required]
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+    }
+}
---
+++ VaporStore/VaporStore/Data/VaporStoreDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaporStore.Data
+{
+    public class VaporStoreDB
+    {
+        public static string ConnectionString = @"Server=DESKTOP-BRJ6BRQ;Database=VaporStoreDB;Trusted_Connection=True";
+    }
+}
