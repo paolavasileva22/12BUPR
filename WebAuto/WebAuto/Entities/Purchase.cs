4585930ab469875564f43f273c2add447c4236f2
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2023-01-11 09:34:34 +0200

    webauto

---
+++ WebAuto/WebAuto/Entities/Purchase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAuto.Entities
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        [ForeignKey("Car")]
+        [Required]
+        public int CarId { get; set; }
+
+        public Car Car { get; set; }
+        [ForeignKey("Client")]
+        [Required]
+        public int ClientId { get; set; }
+
+        public Client Client { get; set; }
+        [Required]
+
+        public DateTime DatePurchase { get; set; }
+    }
+}
