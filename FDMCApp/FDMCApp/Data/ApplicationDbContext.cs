4f1821f2b61ecd97ae38cf1fec2374dfb4a6ada0
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2023-01-13 11:24:57 +0200

    fdcm

---
+++ FDMCApp/FDMCApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FDMCApp.Models;
+
+namespace FDMCApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<FDMCApp.Models.Cat> Cat { get; set; }
+    }
+}
